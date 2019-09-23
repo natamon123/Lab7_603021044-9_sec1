e325fb16ef83e5c72ca1fac9d8c2a9c4c81973e4
BookChanathip <54530222+BookChanathip@users.noreply.github.com>
2019-09-23 17:40:31 +0700

    natamon

---
+++ app/src/main/java/com/example/lab7dialogrv_sec1_603021044_9/StudentsAdapter.kt
@@ -0,0 +1,39 @@
+
+
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.lab7dialogrv_sec1_603021044_9.R
+import com.myweb.lab7dialogrv.Student
+import kotlinx.android.synthetic.main.std_item_layout.view.*
+
+class StudentsAdapter(val items : List<Student>, val context: Context) : RecyclerView.Adapter<ViewHolder>(){
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.tvID?.text= "ID : "+ items[position].id
+        holder.tvName?.text= "Name : "+ items[position].name
+        holder.tvAge?.text= "Age : "+ items[position].age.toString()
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view_item = LayoutInflater.from(parent.context).inflate(R.layout.std_item_layout, parent, false)
+        return ViewHolder(view_item)
+
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+
+    }
+
+    
+}
+
+class ViewHolder (view: View) :RecyclerView.ViewHolder (view) {
+    val tvID= view.tv_id
+    val tvName = view.tv_name
+    val tvAge= view.tv_age
+}
